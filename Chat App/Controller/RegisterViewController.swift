c1f0fc76d5b08a953a07f3e0509dd4beccc0274e
Darvin Sinaga <nagacoder@outlook.com>
2019-07-14 19:07:43 +0700

    Add UI Register

---
+++ Chat App/Controller/RegisterViewController.swift
@@ -0,0 +1,26 @@
+//
+//  RegisterViewController.swift
+//  Chat App
+//
+//  Created by Darvin Sinaga on 14/07/19.
+//  Copyright © 2019 Darvin Sinaga. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    @IBOutlet weak var emailField: PaddingTextField!
+    @IBOutlet weak var passWordField: PaddingTextField!
+    @IBOutlet weak var RegisterButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        emailField.layer.cornerRadius = 20
+        passWordField.layer.cornerRadius = 20
+        RegisterButton.layer.cornerRadius = 25
+    }
+    
+    
+}
